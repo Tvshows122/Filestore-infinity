aa89fa22b3d89c12402cb776e8f3a4fc50b43919
Tvshows122 <126122240+Tvshows122@users.noreply.github.com>
2023-08-06 09:37:19 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -6,4 +6,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Hosted by WD bots - https://t.me/whiteDEvilBots")
+    return web.json_response("Hosted by Tvshows122 - https://t.me/tvshows122")
